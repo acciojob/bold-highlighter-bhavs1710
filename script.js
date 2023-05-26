6bc9fa9455420fe6349b84d0d7737ed0aca646bc
Bhawna Sharma <119921762+bhavs1710@users.noreply.github.com>
2023-05-26 20:27:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 function highlight() {
     //Write your code here
+    let strong = document.getElementsByTagName("strong")
 
+	for(let i = 0 ; i <strong.length ; i++){
+        strong[i].style.color="green"
 
+    }
 }
-
-
 function return_normal() {
-    //Write your code here
+    let strong = document.getElementsByTagName("strong")
+	for(let i = 0 ; i <strong.length ; i++){
+        strong[i].style.color="black"
 
-    
+    }
 }
